ffdf3e45f0c615292571c1736187066aecdbde9f
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-28 18:57:06 -0500

    [:sparkles: feature] añade link a los textos de cada project

--- assets/js/n.js
+++ assets/js/n.js
@@ -6,7 +6,6 @@ var project_sec = document.querySelector('#nuevo_proyectos');
     return res.json();
   }).then(function (data) {
     data.forEach(function (repo) {
-        // Ordenarlos de repo por repo.stargazers_count mayor a menor en un array
         console.log(repo)
         repos = data.sort(function (a, b) {
             return b.stargazers_count - a.stargazers_count;
@@ -16,16 +15,18 @@ var project_sec = document.querySelector('#nuevo_proyectos');
     console.log(repos);
     for (let i = 0; i < 6; i++) {
         project_sec.innerHTML += `
-<div class="col-lg-4 mb-5 mb-lg-0 aos-init aos-animate" data-aos="fade-up">
-              <div class="position-relative">
-                  <span class="d-block position-absolute start-50 top-0 translate-middle-x lh-1 display-1 opacity-25">0${i+1}</span>
-                  <div class="position-relative pt-4 pt-lg-6">
-                      <h3 class="mb-4 h4">${repos[i].name}</h3>
-                      <p class="mb-0 opacity-75 w-lg-75">${repos[i].description}
-                      </p>
-                  </div>
-              </div>
-          </div>`
+        <div class="col-lg-4 mb-5 mb-lg-0 aos-init aos-animate" data-aos="fade-up">
+        <div class="position-relative">
+            <span class="d-block position-absolute start-50 top-0 translate-middle-x lh-1 display-1" style="opacity: .2;">0${i+1}</span>
+            <div class="position-relative pt-4 pt-lg-6">
+                <a class="mb-4 h4 link-hover-underline text-white" href="${repos[0].url}" alt="${repos[0].description}" target="_blank"
+                rel="noreferrer noopener" >${repos[i].name}</a>
+                <p class="mb-0 opacity-75 w-lg-75">${repos[i].description}
+                </p>
+            </div>
+        </div>
+    </div>   `
     }
   });
 }; // vieww more projects
+repos = []
