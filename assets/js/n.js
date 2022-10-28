0af8ee6331a1430166d31d0517ab6812fb335642
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-28 18:50:52 -0500

    [:sparkles: feature] añade seccion de proyectos

--- assets/js/n.js
+++ assets/js/n.js
@@ -1,12 +1,31 @@
-var fetch_url = 'https://api.github.com/users/Bryan-Herrera-Dev/repos?per_page=10&sort=pushed&type=public';
-var project_sec = document.querySelector('#project_sec_ul');
-
+var fetch_url = 'https://api.github.com/users/Bryan-Herrera-Dev/repos?per_page=60';
+let repos;
 window.onload = function () {
+var project_sec = document.querySelector('#nuevo_proyectos');
   fetch(fetch_url).then(function (res) {
     return res.json();
   }).then(function (data) {
     data.forEach(function (repo) {
-      console.log(repo)
+        // Ordenarlos de repo por repo.stargazers_count mayor a menor en un array
+        console.log(repo)
+        repos = data.sort(function (a, b) {
+            return b.stargazers_count - a.stargazers_count;
+        });
+       
     });
+    console.log(repos);
+    for (let i = 0; i < 6; i++) {
+        project_sec.innerHTML += `
+<div class="col-lg-4 mb-5 mb-lg-0 aos-init aos-animate" data-aos="fade-up">
+              <div class="position-relative">
+                  <span class="d-block position-absolute start-50 top-0 translate-middle-x lh-1 display-1 opacity-25">0${i+1}</span>
+                  <div class="position-relative pt-4 pt-lg-6">
+                      <h3 class="mb-4 h4">${repos[i].name}</h3>
+                      <p class="mb-0 opacity-75 w-lg-75">${repos[i].description}
+                      </p>
+                  </div>
+              </div>
+          </div>`
+    }
   });
 }; // vieww more projects
