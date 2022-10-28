6ad6a4322ef68c7deb535548213d1d5fc7540593
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-28 18:07:43 -0500

    [:hammer: refactor] arreglando metatags

---
+++ assets/js/n.js
@@ -0,0 +1,12 @@
+var fetch_url = 'https://api.github.com/users/Bryan-Herrera-Dev/repos?per_page=100';
+var project_sec = document.querySelector('#project_sec_ul');
+
+window.onload = function () {
+  fetch(fetch_url).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    data.forEach(function (repo) {
+      console.log(repo)
+    });
+  });
+}; // vieww more projects
